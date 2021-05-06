8d94fdafea85259e83939e1093b32be92a54c16f
Kirill Vorozhishchev <1507kirill1998@mail.ru>
2021-05-06 22:06:45 +0700

    Add project files.

---
+++ HomeWork_14.3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+   /* std::cout << "Enter your name\n";
+    std::string name;
+    std::cin >> name;*/
+
+    std::string name = "Kirill";
+    std::cout << name << "\n";
+    std::cout << name.length() << "\n";
+    
+    return 0;
+}
